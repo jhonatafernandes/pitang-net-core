8ef00239eaa34dad0cc51908f57a52a794d3f251
Jhonata Fernandes <jhfernandes.oliveira@gmail.com>
2020-04-13 17:56:29 -0300

    controller adds

---
+++ Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace sms_pitang_netcore.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O campo deve ser maior ou igual a 1")]
+        public int IdOwner { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O campo deve ser maior ou igual a 1")]
+        public int IdTarget { get; set; }
+
+    }
+}
---
+++ Models/Messages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace sms_pitang_netcore.Models
+{
+    public class Messages
+    {
+        public Messages()
+        {
+            this.Publicate = DateTime.Now;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres.")]
+        public string Message { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O campo deve ser maior ou igual a 1")]
+        public int IdOwner { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O campo deve ser maior ou igual a 1")]
+        public int IdTarget { get; set; }
+
+
+        public DateTime Publicate { get; set; }
+    }
+}
